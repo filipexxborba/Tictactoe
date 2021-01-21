479e3f8c7c93058486c6a61a361e2228dde09b8c
Filipe Borba <48929981+filipexxborba@users.noreply.github.com>
2021-01-21 13:27:42 -0300

    win effect & restart button

--- js/app.js
+++ js/app.js
@@ -2,6 +2,18 @@ var currentPlayer = 0;
 var blocksArray = document.querySelectorAll('p');
 var winner = 0;
 var checkRunning = 0;
+var jogadas = 0;
+
+function resetGame(){
+    currentPlayer = 0;
+    for(let i = 0; i<9 ; i++){
+        blocksArray[i].innerText = "";
+        blocksArray[i].classList.remove('winner-positions');
+    }
+    jogadas = 0;
+    winner = 0;
+    checkRunning = 0;
+}
 
 function gameAction(element){
     validateState(element);
@@ -21,6 +33,7 @@ function validateBlock(element){
     }else{
         if(element.innerText == ""){
             validatePlayer(element);
+            jogadas++;
         }else{
             console.log('Bloco já selecionado');
         }
@@ -40,7 +53,7 @@ function validatePlayer(element){
 }
 
 function rules(){
-    // Horizontal X
+    // Horizontal o
     if(blocksArray[0].innerText == "O" && blocksArray[1].innerText == "O" && blocksArray[2].innerText == "O"){
     
         blocksArray[0].classList.add('winner-positions');
@@ -49,6 +62,7 @@ function rules(){
     
         checkRunning = 1;
         winner = 1;
+        callWinner();
     }
     if(blocksArray[3].innerText == "O" && blocksArray[4].innerText == "O" && blocksArray[5].innerText == "O"){
     
@@ -58,6 +72,7 @@ function rules(){
     
         checkRunning = 1;
         winner = 1;
+        callWinner();
     }
     if(blocksArray[6].innerText == "O" && blocksArray[7].innerText == "O" && blocksArray[8].innerText == "O"){
     
@@ -67,8 +82,9 @@ function rules(){
     
         checkRunning = 1;
         winner = 1;
+        callWinner();
     }
-    // Vertical X
+    // Vertical o
     if(blocksArray[0].innerText == "O" && blocksArray[3].innerText == "O" && blocksArray[6].innerText == "O"){
     
         blocksArray[0].classList.add('winner-positions');
@@ -77,6 +93,7 @@ function rules(){
     
         checkRunning = 1;
         winner = 1;
+        callWinner();
     }
     if(blocksArray[1].innerText == "O" && blocksArray[4].innerText == "O" && blocksArray[7].innerText == "O"){
     
@@ -86,6 +103,7 @@ function rules(){
     
         checkRunning = 1;
         winner = 1;
+        callWinner();
     }
     if(blocksArray[2].innerText == "O" && blocksArray[5].innerText == "O" && blocksArray[8].innerText == "O"){
     
@@ -95,6 +113,7 @@ function rules(){
     
         checkRunning = 1;
         winner = 1;
+        callWinner();
     }
     // Diagonais
     if(blocksArray[0].innerText == "O" && blocksArray[4].innerText == "O" && blocksArray[8].innerText == "O"){
@@ -105,6 +124,7 @@ function rules(){
     
         checkRunning = 1;
         winner = 1;
+        callWinner();
     }
     if(blocksArray[6].innerText == "O" && blocksArray[4].innerText == "O" && blocksArray[2].innerText == "O"){
     
@@ -114,9 +134,10 @@ function rules(){
     
         checkRunning = 1;
         winner = 1;
+        callWinner();
     }
     
-    // Horizontal O
+    // Horizontal X
     if(blocksArray[0].innerText == "X" && blocksArray[1].innerText == "X" && blocksArray[2].innerText == "X"){
     
         blocksArray[0].classList.add('winner-positions');
@@ -124,6 +145,7 @@ function rules(){
         blocksArray[2].classList.add('winner-positions');
         checkRunning = 1;
         winner = 2;
+        callWinner();
     }
     if(blocksArray[3].innerText == "X" && blocksArray[4].innerText == "X" && blocksArray[5].innerText == "X"){
         blocksArray[3].classList.add('winner-positions');
@@ -131,6 +153,7 @@ function rules(){
         blocksArray[5].classList.add('winner-positions');
         checkRunning = 1;
         winner = 2;
+        callWinner();
     }
     if(blocksArray[6].innerText == "X" && blocksArray[7].innerText == "X" && blocksArray[8].innerText == "X"){
         blocksArray[6].classList.add('winner-positions');
@@ -138,14 +161,16 @@ function rules(){
         blocksArray[8].classList.add('winner-positions');
         checkRunning = 1;
         winner = 2;
+        callWinner();
     }
-    // Vertical O
+    // Vertical X
     if(blocksArray[0].innerText == "X" && blocksArray[3].innerText == "X" && blocksArray[6].innerText == "X"){
         blocksArray[0].classList.add('winner-positions');
         blocksArray[3].classList.add('winner-positions');
         blocksArray[6].classList.add('winner-positions');
         checkRunning = 1;
         winner = 2;
+        callWinner();
     }
     if(blocksArray[1].innerText == "X" && blocksArray[4].innerText == "X" && blocksArray[7].innerText == "X"){
         blocksArray[1].classList.add('winner-positions');
@@ -153,6 +178,7 @@ function rules(){
         blocksArray[7].classList.add('winner-positions');
         checkRunning = 1;
         winner = 2;
+        callWinner();
     }
     if(blocksArray[2].innerText == "X" && blocksArray[5].innerText == "X" && blocksArray[8].innerText == "X"){
         blocksArray[2].classList.add('winner-positions');
@@ -160,6 +186,7 @@ function rules(){
         blocksArray[8].classList.add('winner-positions');
         checkRunning = 1;
         winner = 2;
+        callWinner();
     }
     // Diagonais
     if(blocksArray[0].innerText == "X" && blocksArray[4].innerText == "X" && blocksArray[8].innerText == "X"){
@@ -168,6 +195,7 @@ function rules(){
         blocksArray[8].classList.add('winner-positions');
         checkRunning = 1;
         winner = 2;
+        callWinner();
     }
     if(blocksArray[6].innerText == "X" && blocksArray[4].innerText == "X" && blocksArray[2].innerText == "X"){
         blocksArray[6].classList.add('winner-positions');
@@ -175,6 +203,45 @@ function rules(){
         blocksArray[2].classList.add('winner-positions');
         checkRunning = 1;
         winner = 2;
+        callWinner();
+    }
+    if(jogadas == 9){
+        winner = 3;
+        jogadas = 0;
+        callWinner();
+    }
+}
+
+function callWinner(){
+    if(winner == '1'){
+        document.querySelector('#text-winner').innerText = "O vencedor é o player O!";
+        document.querySelector('.wrapper').classList.add('blur');
+        document.querySelector('.winner-box').classList.add('active');
+        setTimeout(() => {
+            document.querySelector('.winner-box').classList.remove('active');
+            document.querySelector('.wrapper').classList.remove('blur');
+            winner = 0;
+        }, 3000);
+    }
+    if(winner == '2'){
+        document.querySelector('#text-winner').innerText = "O vencedor é o player X!";
+        document.querySelector('.wrapper').classList.add('blur');
+        document.querySelector('.winner-box').classList.add('active');
+        setTimeout(() => {
+            document.querySelector('.winner-box').classList.remove('active');
+            document.querySelector('.wrapper').classList.remove('blur');
+            winner = 0;
+        }, 3000);
+    }
+    if(winner == '3'){
+        document.querySelector('#text-winner').innerText = "Empate!";
+        document.querySelector('.wrapper').classList.add('blur');
+        document.querySelector('.winner-box').classList.add('active');
+        setTimeout(() => {
+            document.querySelector('.winner-box').classList.remove('active');
+            document.querySelector('.wrapper').classList.remove('blur');
+            winner = 0;
+        }, 3000);
     }
 }
 
